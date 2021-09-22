03e2085a3d84f298e1c45584e4b9a0a819ed45ec
Angelo Gatz <angeloagatz@gmail.com>
2021-09-22 08:35:00 -0300

    correção pagina produtos

--- js/main.js
+++ js/main.js
@@ -28,24 +28,6 @@ function reveal(){
     }
 }
 
-window.addEventListener('load', revealspec);
-
-function revealspec(){
-    var reveals = document.querySelectorAll('.revealspec');
-
-    for(var i = 0; i < reveals.length; i++){
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
-
-        if(revealtop < windowheight - revealpoint){
-            reveals[i].classList.add('active');
-        }
-        else{
-            reveals[i].classList.remove('active');
-        }
-    }
-}
 
 window.addEventListener('load', reveal_social);
 
